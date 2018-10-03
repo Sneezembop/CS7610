412898a4494f554eb5631b670fea5d2b35ec2891
Miles Benjamin <louren.of.courone@gmail.com>
2018-10-03 15:23:35 -0400

    Multiple casters works!

--- Project1/data/prj1_tm.py
+++ Project1/data/prj1_tm.py
@@ -4,6 +4,7 @@ import socket
 import csv
 import pickle
 import random
+import pprint as pp
 
 # command line args: speaker (bool)
 
@@ -15,9 +16,10 @@ myIP = None
 myNetworkData = []
 iAmASpeaker = False
 seq = 1
-myMessageQueue = []
+myDataMessageQueue = []
 Delivered = []
-MessageCount = 4
+MessageCount = 5
+outgoingMessageQueue = []
 
 def setUp():
     global myHostName
@@ -74,9 +76,9 @@ def printGlobals():
     return
 
 def openListeningPort():
-    listeningSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    listeningSocket.bind((myIP, int(myPort)))
-    return listeningSocket
+    mysocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    mysocket.bind((myIP, int(myPort)))
+    return mysocket
 
 def closeSocket(mySocket):
     mySocket.close()
@@ -84,11 +86,29 @@ def closeSocket(mySocket):
 
 def broadcast(message):
     global myNetworkData
+    global outgoingMessageQueue
+
+    outgoingMessageQueue.append((message, myIP, myPort))
 
     for i in myNetworkData:
-        sendMessage(message, i['ip'], i['port'])
+        if i['port'] != myPort:
+            outgoingMessageQueue.append((message, i['ip'], i['port']))
+    
     
     return
+
+def newMessageId():
+    global seq
+    global myDataMessageQueue
+
+    temp = seq
+
+    for i in myDataMessageQueue:
+        temp = max(temp, i['msg_id'])
+
+    temp = temp + random.randint(1,10)
+    return temp
+
 
 def sendMessage(message, ip, port):
     tempsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -141,83 +161,113 @@ def unpackMessage(message):
 
 def proscessDataMessage(message):
     global seq
-    global myMessageQueue
+    global myDataMessageQueue
+    global outgoingMessageQueue
+
     seq = max(message['msg_id'], seq) + 1
 
     message['deliverable'] = False
     message['seq'] = seq
-    myMessageQueue.append(message)
+    myDataMessageQueue.append(message)
     ackMessage = conAckMessage(message['sender'], message['msg_id'], seq)
     ip, port = getIPandPortbyProcId(message['sender'])
     if ip and port:
-        sendMessage(ackMessage, ip, port)
+        outgoingMessageQueue.append((ackMessage, ip, port))
     else:
         print("ip and port not found, check hostfile")
 
     return
 
 def proscessAckMessage(message):
-    global myMessageQueue
-    for i in myMessageQueue:
+    global myDataMessageQueue
+    for i in myDataMessageQueue:
         if (i['msg_id'] == message['msg_id']):
-            i[message['proposer']] = message['proposed_seq']
-            #i['msg_id'][message['proposer']] = message['proposed_seq']
+            tup = (message['proposer'], message['proposed_seq'])
+            if 'proposed_seqs' not in i.keys():
+                i['proposed_seqs'] = []
+            i['proposed_seqs'].append(tup)
     
     return
 
 
 def proscessSeqMessage(message):
-    global myMessageQueue
+    global myDataMessageQueue
     global Delivered
 
-    for i in myMessageQueue:
+    for i in myDataMessageQueue:
         if i['msg_id'] == message['msg_id']:
             i['seq'] = message['final_seq']
+            i['deliverable'] = True
             Delivered.append(i)
-            Delivered = sorted(Delivered, key=lambda i: i['seq']) 
 
     return
-
+ 
 def SpeakerBehavior():
-    global myMessageQueue
-    global Delivered
+    global myDataMessageQueue
     global MessageCount
+    global seq
 
     unAckdMessage = False
 
-    print(str(myMessageQueue))
-
-    # check to see if we have unacknowledged messages we need to ask about.
-    for i in myMessageQueue:
+    # check to see if we have messages we can deliver.
+    for i in myDataMessageQueue:
         if i['deliverable'] == False:
-            deliverableCheck = True
-            for j in myNetworkData:
-                if not j['processid'] in i.keys():
-                    deliverableCheck = False
-                    ip, port = getIPandPortbyProcId(j['processid'])
-                    sendMessage(conDataMessage(i['seq'], i['data']),ip,port)
-                    unAckdMessage = True
-                    break
-            if deliverableCheck:
+            print(i)
+            if 'proposed_seqs' in i.keys() and len(i['proposed_seqs']) == len(myNetworkData):
                 i['deliverable'] = True
                 i['seq'] = getMaxSeq(i)
                 broadcast(conSeqMessage(i['sender'],i['msg_id'],i['seq']))
+            else:
+                unAckdMessage = True
 
 
-    if MessageCount >= 0 and not unAckdMessage:
-        broadcast(conDataMessage(seq +1, random.randint(1,50)))
-        MessageCount -= 1
-    if MessageCount <= 0:
-        broadcast(conTerMessage())
+    if MessageCount > 0 and not unAckdMessage:
+        seq = seq + 1
+        broadcast(conDataMessage(newMessageId(), random.randint(1,50)))
+        MessageCount  = MessageCount - 1
+        print("broadcasting message count" + str(MessageCount))
+    elif (MessageCount == 0):
+       # broadcastTermination()
+        MessageCount  = MessageCount - 1
+
+
+
+    return
+
+def broadcastTermination():
+    global myNetworkData
+    global outgoingMessageQueue
+
+    message = conTerMessage()
+
+    for i in myNetworkData:
+        if i['port'] != myPort:
+            outgoingMessageQueue.append((message, i['ip'], i['port']))
+    
+    outgoingMessageQueue.append((message, myIP, myPort))
+        
+    return
+
+
+def processOutgoingMessage(outgoingQueue):
+
+    messageData = outgoingQueue.pop(0)
+    message = messageData[0]
+    ip = messageData[1]
+    port = messageData[2]
+
+    temp = unpackMessage(message)
+    print("sending message: " + str(temp) + " to port " + str(port))
+
+    sendMessage(message,ip, port)
 
     return
 
 
 def getMaxSeq(message):
     temp = []
-    for i in range(len(myNetworkData)):
-        if i in message.keys():
-            temp.append(message[i])
+    for i in message['proposed_seqs']:
+        temp.append(i[1])
 
     if len(temp) > 0:
         return max(temp)
@@ -226,44 +276,28 @@ def getMaxSeq(message):
     
 
 
-def listenerBehavior():
-    mysocket = openListeningPort()
+def listen(mysocket):
+    
 
-    done = False
-
-    global seq
-    global myMessageQueue
-
-    while not done:
-
-        if iAmASpeaker:
-            SpeakerBehavior()
-
+    try:
         temp = mysocket.recv(4096)
-
-        if len(temp) > 0:
-            temp = unpackMessage(temp)
-            print(temp)
-
-            if temp['type'] == 1:
-                
-                proscessDataMessage(temp)
-            elif temp['type'] == 2:
-                proscessAckMessage(temp)
-
-            elif temp['type'] == 3:
-                
-                proscessSeqMessage(temp)
-
-            elif temp['type'] == 4:
-                done = True
+    except socket.error as msg:
+        temp = []
 
 
-            temp = None
-            
-    closeSocket(mysocket)
-    return
+    if len(temp) > 0:
+        temp = unpackMessage(temp)
+        print(temp)
 
+        if temp['type'] == 1:        
+            proscessDataMessage(temp)
+        elif temp['type'] == 2:
+            proscessAckMessage(temp)
+        elif temp['type'] == 3:     
+            proscessSeqMessage(temp)
+        elif temp['type'] == 4:
+            return True
+    return False
 
 
 def main():
@@ -273,10 +307,46 @@ def main():
         print("I am a sender")
     else:
         print("I am a reciever")
+
+    done = False
+
+    global seq
+    global myDataMessageQueue
+    global outgoingMessageQueue
+    global Delivered
+
+    mysocket = openListeningPort()
+    mysocket.settimeout(5)
+
+    while not done:
+
+        listen(mysocket)
+        mysocket.settimeout(0.5)
+
+        if len(outgoingMessageQueue) > 0:
+            processOutgoingMessage(outgoingMessageQueue)
+        else:
+            if iAmASpeaker: 
+                SpeakerBehavior()
+
+        done = (len(Delivered) == 10 and (len(outgoingMessageQueue) == 0))
     
-    listenerBehavior()
-    print(str(Delivered))
+    closeSocket(mysocket)
+    
+    printOutput(Delivered)
+
     return
+
+
+def printOutput(deliveredList):
+    deliveredList = sorted(deliveredList, key=lambda i: i['seq'])
+
+    pp.pprint(deliveredList)
+    output = ""
+    for i in deliveredList:
+        output += str(i['seq']) + ", "
+
+    pp.pprint("Final Sequence: " + output)
 
 
 main()
